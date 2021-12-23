dda98ff2057b62861ef06519a229ddf8e678a93c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-12-23 15:13:04 +0900

    Update Closest-Numbers.cpp

--- Closest-Numbers.cpp
+++ Closest-Numbers.cpp
@@ -1,5 +1,33 @@
+// from https://www.hackerrank.com/challenges/closest-numbers/forum by bbm833
 vector<int> closestNumbers(vector<int> arr)
 {
-    vector<int> result {-20, 30};
+    std::vector<int> result{}; 
+
+    if (arr.size() == 0)
+    {
+        return result;
+    }
+
+    std::sort(arr.begin(), arr.end());
+    int min = INT32_MAX;
+
+    for (int i = 0; i < arr.size()-1; ++i)
+    {
+        const int difference = arr[i+1] - arr[i];
+        
+        if (difference < min)
+        {
+            result.clear();
+            result.push_back(arr[i]);
+            result.push_back(arr[i+1]);
+            min = difference;
+        }
+        else if (difference == min)
+        {
+            result.push_back(arr[i]);
+            result.push_back(arr[i+1]);
+        }
+    }
+
     return result;
 }
