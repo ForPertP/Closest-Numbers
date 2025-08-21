901ebc139f5a3b78c47f730df08b9313b63c9aa6
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-08-22 06:15:49 +0900

    Update Closest-Numbers.cpp

--- Closest-Numbers.cpp
+++ Closest-Numbers.cpp
@@ -17,6 +17,30 @@ vector<int> closestNumbers(vector<int> arr) {
 
 }
 
+vector<int> closestNumbers(vector<int> arr)
+{
+    vector<int> result;
+    if (arr.size() < 2) return result;
+
+    sort(arr.begin(), arr.end());
+
+    vector<int> diffs(arr.size() - 1);
+    transform(arr.begin(), arr.end() - 1, arr.begin() + 1, diffs.begin(),
+              [](int a, int b) { return b - a; });
+
+    int minDiff = *min_element(diffs.begin(), diffs.end());
+
+    for (size_t i = 0; i < diffs.size(); ++i)
+    {
+        if (diffs[i] == minDiff)
+        {
+            result.push_back(arr[i]);
+            result.push_back(arr[i + 1]);
+        }
+    }
+    return result;
+}
+
 // from https://www.hackerrank.com/challenges/closest-numbers/forum by bbm833
 vector<int> closestNumbers2(vector<int> arr)
 {
