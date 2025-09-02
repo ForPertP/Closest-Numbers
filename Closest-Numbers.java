62f85c42dc215ad2c33ba885ab3627daaef1a3db
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-09-03 06:57:28 +0900

    Implement skeleton for closestNumbers function

--- Closest-Numbers.java
+++ Closest-Numbers.java
@@ -9,3 +9,21 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'closestNumbers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> closestNumbers(List<Integer> arr) {
+    // Write your code here
+
+    }
+
+}
